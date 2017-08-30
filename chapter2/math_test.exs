260c2fe1ae2f3e157f8bd50bdfa71905e8d15c0d
eiji mishiro <eiji346g@gmail.com>
2017-08-30 11:09:07 +0900

    add test case

--- chapter2/math_test.exs
+++ chapter2/math_test.exs
@@ -2,12 +2,36 @@ defmodule MathTest do
   use Assertion
   test "integers can be added and subtracted" do
     assert 1 + 1 ==2
-    assert 2 + 3 == 5
     assert 5 - 5 == 10
   end
-
   test "integers can be multiplied and divided" do
     assert 5 * 5 == 25
     assert 10 / 5 == 5
+    assert 10 / 5 == 1
+  end
+  test "!=, ===, !==" do
+    assert 4 != 2
+    assert 4 != 4
+    assert 4 == 4.0
+    assert 4 === 4
+    assert 4 === 4.0
+    assert 4 !== 4
+    assert 4 !== 4.0
+  end
+  test ">, =>" do
+    assert 4 > 2
+    assert 4 > 4
+    assert 4 > 5
+    assert 4 >= 2
+    assert 4 >= 4
+    assert 4 >= 5
+  end
+  test "<, <=" do
+    assert 4 < 2
+    assert 4 < 4
+    assert 4 < 5
+    assert 4 <= 2
+    assert 4 <= 4
+    assert 4 <= 5
   end
 end
