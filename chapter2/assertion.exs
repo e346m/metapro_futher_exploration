6989ee2ec675d215f34914ebc38787dad054cba8
eiji mishiro <eiji346g@gmail.com>
2017-08-31 11:32:26 +0900

    add refute

--- chapter2/assertion.exs
+++ chapter2/assertion.exs
@@ -22,17 +22,24 @@ defmodule Assertion do
   end
   defmacro assert({opr, _, [lhs, rhs]}) do
     quote bind_quoted: [opr: opr, lhs: lhs, rhs: rhs] do
-      Assertion.Test.assert(opr, lhs, rhs)
+      Assertion.Test.assert(opr, lhs, rhs, :assert)
+    end
+  end
+  defmacro refute({opr, _, [lhs, rhs]}) do
+    quote bind_quoted: [opr: opr, lhs: lhs, rhs: rhs] do
+      Assertion.Test.assert(opr, lhs, rhs, :refute)
     end
   end
 end
 
 defmodule Assertion.Test do
+  @fail "It must be false or nil"
   def run(tests, module) do
     Enum.each tests, fn {test_func, description} ->
       case apply(module, test_func, []) do
         :ok -> IO.write "."
-        {:fail, reason} -> IO.puts"""
+        {:fail, reason} -> IO.puts """
+
           =======================
           FAILURE: #{description}
           =======================
@@ -41,94 +48,98 @@ defmodule Assertion.Test do
       end
     end
   end
-  def assert(boolean) do
-    cond do
-      boolean == true -> :ok
-      boolean == false -> {
-        :fail, """
-        It's false!
-        """
+  def check(boolean, label) do
+    case label do
+      :assert -> boolean
+      :refute -> !boolean
+    end
+  end
+  def assert(boolean, label, message \\ @fail) do
+    case check(boolean, label) do
+      true -> :ok
+      false -> {
+        :fail,  message
       }
     end
   end
-  def assert(:==, lhs, rhs) when lhs == rhs do
-    :ok
+  def assert(:==, lhs, rhs, label) when lhs == rhs do
+    assert(true, label)
   end
-  def assert(:==, lhs, rhs) do
-    {:fail, """
+  def assert(:==, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       to be equal to: #{rhs}
       """
-    }
+    )
   end
-  def assert(:===, lhs, rhs) when lhs === rhs do
-    :ok
+  def assert(:===, lhs, rhs, label) when lhs === rhs do
+    assert(true, label)
   end
-  def assert(:===, lhs, rhs) do
-    {:fail, """
+  def assert(:===, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       to be exactly equal to: #{rhs}
       """
-    }
+    )
   end
-  def assert(:!=, lhs, rhs) when lhs != rhs do
-    :ok
+  def assert(:!=, lhs, rhs, label) when lhs != rhs do
+    assert(true, label)
   end
-  def assert(:!=, lhs, rhs) do
-    {:fail, """
+  def assert(:!=, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       not to be equal to: #{rhs}
       """
-    }
+    )
   end
-  def assert(:!==, lhs, rhs) when lhs !== rhs do
-    :ok
+  def assert(:!==, lhs, rhs, label) when lhs !== rhs do
+    assert(true, label)
   end
-  def assert(:!==, lhs, rhs) do
-    {:fail, """
+  def assert(:!==, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       not to be exactly equal to: #{rhs}
       """
-    }
+    )
   end
-  def assert(:>, lhs, rhs) when lhs > rhs do
-    :ok
+  def assert(:>, lhs, rhs, label) when lhs > rhs do
+    assert(true, label)
   end
-  def assert(:>, lhs, rhs) do
-    {:fail, """
+  def assert(:>, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       to be greater than: #{rhs}
       """
-    }
+    )
   end
-  def assert(:>=, lhs, rhs) when lhs >= rhs do
-    :ok
+  def assert(:>=, lhs, rhs, label) when lhs >= rhs do
+    assert(true, label)
   end
-  def assert(:>=, lhs, rhs) do
-    {:fail, """
+  def assert(:>=, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       to be equal to or greater than: #{rhs}
       """
-    }
+    )
   end
-  def assert(:<, lhs, rhs) when lhs < rhs do
-    :ok
+  def assert(:<, lhs, rhs, label) when lhs < rhs do
+    assert(true, label)
   end
-  def assert(:<, lhs, rhs) do
-    {:fail, """
+  def assert(:<, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       to be less than: #{rhs}
       """
-    }
+    )
   end
-  def assert(:<=, lhs, rhs) when lhs <= rhs do
-    :ok
+  def assert(:<=, lhs, rhs, label) when lhs <= rhs do
+    assert(true, label)
   end
-  def assert(:<=, lhs, rhs) do
-    {:fail, """
+  def assert(:<=, lhs, rhs, label) do
+    assert(false, label, """
       Expected: #{lhs}
       to be equal to or greater than: #{rhs}
       """
-    }
+    )
   end
 end
